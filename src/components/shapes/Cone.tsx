e9076856e53f11a03d170e6bb517626682c46e82
Bernardo Domingues <bernardoopd@hotmail.com>
2025-06-09 19:02:01 -0300

    fix: cone visualization

--- src/components/shapes/Cone.tsx
+++ src/components/shapes/Cone.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import * as THREE from "three";
+
 export default function Cone({
   showEdges,
   showFaces,
@@ -35,6 +37,13 @@ export default function Cone({
           <pointsMaterial color="green" size={0.2} />
         </points>
       )}
+      <group>
+        {/* Bottom circle */}
+        <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <ringGeometry args={[0.99, 1.01, 32]} />
+          <lineBasicMaterial color="white" side={THREE.DoubleSide} />
+        </mesh>
+      </group>
     </mesh>
   );
 } 
